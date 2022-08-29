339fa77692f9810305ff3ea0279fbf33d18c597b
bd_ <bdunderscore@fushizen.net>
2022-08-29 16:28:53 -0700

    Revert "Remove bone proxy"
    This reverts commit f0fa188d68e1098d53caf17412c0762f5b5fa093.

---
+++ Packages/net.fushizen.modular-avatar/Editor/BoneProxyHook.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+namespace net.fushizen.modular_avatar.core.editor
+{
+    public class BoneProxyHook : IVRCSDKPreprocessAvatarCallback
+    {
+        public int callbackOrder => HookSequence.SEQ_BONE_PROXY;
+        
+        public bool OnPreprocessAvatar(GameObject avatarGameObject)
+        {
+            var boneProxies = avatarGameObject.GetComponentsInChildren<ModularAvatarBoneProxy>(true);
+
+            foreach (var proxy in boneProxies)
+            {
+                if (proxy.constraint != null) UnityEngine.Object.DestroyImmediate(proxy.constraint);
+                if (proxy.target != null)
+                {
+                    var oldPath = RuntimeUtil.AvatarRootPath(proxy.gameObject);
+                    Transform transform = proxy.transform;
+                    transform.SetParent(proxy.target, false);
+                    transform.localPosition = Vector3.zero;
+                    transform.localRotation = Quaternion.identity;
+                    PathMappings.Remap(oldPath, new PathMappings.MappingEntry() {
+                        path = RuntimeUtil.AvatarRootPath(proxy.gameObject),
+                        transformPath = RuntimeUtil.AvatarRootPath(proxy.gameObject)
+                    });
+                }
+                Object.DestroyImmediate(proxy);
+            }
+
+            return true;
+        }
+    }
+}
