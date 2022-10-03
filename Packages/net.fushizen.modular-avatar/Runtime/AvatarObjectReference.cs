f2856b2e09b7b5c219fd16bc07ea9ac7db6c1d86
bd_ <bdunderscore@fushizen.net>
2022-10-02 20:34:39 -0700

    AvatarObjectReference defaults to a null referent
    Fixes #47

--- Packages/net.fushizen.modular-avatar/Runtime/AvatarObjectReference.cs
+++ Packages/net.fushizen.modular-avatar/Runtime/AvatarObjectReference.cs
@@ -6,7 +6,7 @@ namespace net.fushizen.modular_avatar.core
     [Serializable]
     public struct AvatarObjectReference
     {
-        public bool isNull;
+        public static string AVATAR_ROOT = "$$$AVATAR_ROOT$$$";
         public string referencePath;
 
         private bool _cacheValid;
@@ -15,12 +15,12 @@ namespace net.fushizen.modular_avatar.core
 
         public GameObject Get(Component container)
         {
-            if (_cacheValid && _cachedPath == referencePath && !isNull) return _cachedReference;
+            if (_cacheValid && _cachedPath == referencePath) return _cachedReference;
 
             _cacheValid = true;
             _cachedPath = referencePath;
 
-            if (isNull)
+            if (referencePath == "")
             {
                 _cachedReference = null;
                 return _cachedReference;
@@ -31,6 +31,12 @@ namespace net.fushizen.modular_avatar.core
 
             var avatar = RuntimeUtil.FindAvatarInParents(container.transform);
             if (avatar == null) return (_cachedReference = null);
+
+            if (referencePath == AVATAR_ROOT)
+            {
+                _cachedReference = avatar.gameObject;
+                return _cachedReference;
+            }
 
             return (_cachedReference = avatar.transform.Find(referencePath)?.gameObject);
         }
