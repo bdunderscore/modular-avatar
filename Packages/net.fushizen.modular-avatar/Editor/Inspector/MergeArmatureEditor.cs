0c4932d5eae45a0edd078a59c3de9794f43bb324
bd_ <bdunderscore@fushizen.net>
2022-10-04 19:43:46 -0700

    Add a context menu setup option for outfits

--- Packages/net.fushizen.modular-avatar/Editor/Inspector/MergeArmatureEditor.cs
+++ Packages/net.fushizen.modular-avatar/Editor/Inspector/MergeArmatureEditor.cs
@@ -22,33 +22,7 @@ namespace net.fushizen.modular_avatar.core.editor
                                                      && string.IsNullOrEmpty(target.prefix)
                                                      && string.IsNullOrEmpty(target.suffix))
                 {
-                    // We only infer if targeting the armature (below the Hips bone)
-                    var rootAnimator = RuntimeUtil.FindAvatarInParents(target.transform)?.GetComponent<Animator>();
-                    if (rootAnimator == null) return;
-
-                    var hips = rootAnimator.GetBoneTransform(HumanBodyBones.Hips);
-                    if (hips == null || hips.transform.parent != target.mergeTargetObject.transform) return;
-
-                    // We also require that the attached object has exactly one child (presumably the hips)
-                    if (target.transform.childCount != 1) return;
-
-                    // Infer the prefix and suffix by comparing the names of the mergeTargetObject's hips with the child of the
-                    // GameObject we're attached to.
-                    var baseName = hips.name;
-                    var mergeName = target.transform.GetChild(0).name;
-
-                    var prefixLength = mergeName.IndexOf(baseName, StringComparison.InvariantCulture);
-                    if (prefixLength < 0) return;
-
-                    var suffixLength = mergeName.Length - prefixLength - baseName.Length;
-
-                    target.prefix = mergeName.Substring(0, prefixLength);
-                    target.suffix = mergeName.Substring(mergeName.Length - suffixLength);
-
-                    if (!string.IsNullOrEmpty(target.prefix) || !string.IsNullOrEmpty(target.suffix))
-                    {
-                        RuntimeUtil.MarkDirty(target);
-                    }
+                    target.InferPrefixSuffix();
                 }
             }
         }
