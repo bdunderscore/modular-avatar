ecf62d7c8e1d1f3f123a100e1d7cdb57866a69d3
bd_ <bdunderscore@fushizen.net>
2022-11-08 18:07:51 -0800

    Fix favicon reference

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo/ma_logo.png',
+  favicon: 'img/favicon.ico.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
