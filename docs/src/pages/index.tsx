2d9e63c83ab8520ed8c0017e3d2f0826c466a85d
bd_ <bdunderscore@fushizen.net>
2022-11-28 16:54:03 -0800

    Fixing a broken doc link

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -40,7 +40,7 @@ function HomepageHeader() {
           </Link>
           <Link
             className={`button button--secondary button--lg ${styles.button}`}
-            to="/docs/tutorials">
+            to="/docs/tutorials/">
             <Translate>Tutorials</Translate>
           </Link>
         </div>
