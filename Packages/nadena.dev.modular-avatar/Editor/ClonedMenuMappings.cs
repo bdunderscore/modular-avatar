96f65000af57f544f0a9456815b4c63336621fe9
raiti-chan <rusuke@t-net.ne.jp>
2022-12-17 00:06:06 +0900

    Add ClonedMenuMappings

---
+++ Packages/nadena.dev.modular-avatar/Editor/ClonedMenuMappings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+// ReSharper disable once CheckNamespace
+namespace nadena.dev.modular_avatar.core.editor {
+	public static class ClonedMenuMappings {
+		private static Dictionary<VRCExpressionsMenu, ImmutableArray<VRCExpressionsMenu>> ClonedMappings =
+			new Dictionary<VRCExpressionsMenu, ImmutableArray<VRCExpressionsMenu>>();
+
+		private static Dictionary<VRCExpressionsMenu, VRCExpressionsMenu> OriginalMapping =
+			new Dictionary<VRCExpressionsMenu, VRCExpressionsMenu>();
+
+		public static void Clear() {
+			ClonedMappings.Clear();
+			OriginalMapping.Clear();
+		}
+
+		public static void Add(VRCExpressionsMenu original, VRCExpressionsMenu clonedMenu) {
+			if (!ClonedMappings.TryGetValue(original, out ImmutableArray<VRCExpressionsMenu> clonedMenus)) {
+				clonedMenus = ImmutableArray<VRCExpressionsMenu>.Empty;
+			}
+			// Usually, one menu is rarely duplicated in multiple menus, so don't bother using a Builder
+			ClonedMappings[original] = clonedMenus.Add(clonedMenu);
+			OriginalMapping[clonedMenu] = original;
+		}
+
+		public static bool TryGetClonedMenus(VRCExpressionsMenu original, out ImmutableArray<VRCExpressionsMenu> clonedMenus) {
+			return ClonedMappings.TryGetValue(original, out clonedMenus);
+		}
+		
+		public static VRCExpressionsMenu GetOriginal(VRCExpressionsMenu cloned) {
+			return OriginalMapping.TryGetValue(cloned, out VRCExpressionsMenu original) ? original : null;
+		}
+	}
+}
