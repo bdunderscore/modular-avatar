c2e6e0ad89f832f188e0bcbe826930c3948042d2
bd_ <bd_@nadena.dev>
2023-04-09 21:02:05 +0900

    chore: add test for null subparameter field issue

---
+++ Assets/_ModularAvatar/EditModeTests/NullSubparameterTest.cs
@@ -0,0 +1,24 @@
+﻿using nadena.dev.modular_avatar.core.editor;
+using NUnit.Framework;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+namespace modular_avatar_tests
+{
+    public class NullSubparameterTest : TestBase
+    {
+        [Test]
+        public void TestNullSubparametersField()
+        {
+            VRCExpressionsMenu menu = ScriptableObject.CreateInstance<VRCExpressionsMenu>();
+            var root = CreateRoot("root");
+
+            var avatar = root.GetComponent<VRCAvatarDescriptor>();
+            avatar.expressionsMenu = menu;
+
+            // This should not throw an exception
+            ParameterPolicy.ProbeParameters(root);
+        }
+    }
+}
