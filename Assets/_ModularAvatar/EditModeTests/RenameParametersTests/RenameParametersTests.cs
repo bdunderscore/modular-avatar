a4c3e1d0f44602baada647b4c5ebf290026a5d6d
bd_ <bd_@nadena.dev>
2023-05-23 21:10:22 +0900

    fix: internal parameters not renamed on installed VRCExpressionMenu submenus (#314)
    Thanks to suzuryg for finding the cause of the bug and suggesting an initial fix.
    
    Reported-By: 33linn
    Fixes: #305

--- Assets/_ModularAvatar/EditModeTests/RenameParametersTests/RenameParametersTests.cs
+++ Assets/_ModularAvatar/EditModeTests/RenameParametersTests/RenameParametersTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace modular_avatar_tests.RenameParametersTests
 {
@@ -31,6 +32,35 @@ namespace modular_avatar_tests.RenameParametersTests
             {
                 Assert.AreEqual("param_new", driver.parameters[0].name);
             }
+        }
+
+        [Test]
+        public void RenameInstalledMenu()
+        {
+            var prefab = CreatePrefab("RenameInstalledMenu.prefab");
+            AvatarProcessor.ProcessAvatar(prefab);
+
+            var menu = prefab.GetComponent<VRCAvatarDescriptor>().expressionsMenu;
+            Assert.AreEqual("test$$Internal_0", menu.controls[0].parameter.name);
+            Assert.AreEqual("test$$Internal_0", menu.controls[1].subMenu.controls[0].parameter.name);
+        }
+
+        [Test]
+        public void TestRecursiveMenu()
+        {
+            var menu = LoadAsset<VRCExpressionsMenu>("m1.asset");
+            var avatar = CreateRoot("root");
+            var child = CreateChild(avatar, "child");
+            var param = child.AddComponent<ModularAvatarParameters>();
+            param.parameters.Add(new ParameterConfig()
+            {
+                nameOrPrefix = "test",
+                internalParameter = true,
+            });
+            var installer = child.AddComponent<ModularAvatarMenuInstaller>();
+            installer.menuToAppend = menu;
+
+            AvatarProcessor.ProcessAvatar(avatar);
         }
     }
 }
