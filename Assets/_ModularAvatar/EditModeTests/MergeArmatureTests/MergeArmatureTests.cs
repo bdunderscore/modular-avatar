4c9ec6908317acb8290ad7b00034358d166c4848
bd_ <bd_@nadena.dev>
2023-06-05 20:40:48 +0900

    fix: Don't strip objects which contain unrecognized components
    Fixes: #321

---
+++ Assets/_ModularAvatar/EditModeTests/MergeArmatureTests/MergeArmatureTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using modular_avatar_tests;
+using nadena.dev.modular_avatar.core.editor;
+using NUnit.Framework;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+public class MergeArmatureTests : TestBase
+{
+    [Test]
+    public void DontStripObjectsWithComponents()
+    {
+        var root = CreatePrefab("ColliderMergeTest.prefab");
+
+        AvatarProcessor.ProcessAvatar(root);
+
+        // We expect two children:  Mergable and Hips$[uuid] (retained due to the BoxCollider)
+        var targetHips = root.transform.Find("TargetArmature/Hips");
+        Assert.AreEqual(2, targetHips.childCount);
+        Assert.AreEqual("Mergable", targetHips.GetChild(0).gameObject.name);
+
+        Assert.NotNull(targetHips.GetChild(1).GetComponent<BoxCollider>());
+    }
+}
