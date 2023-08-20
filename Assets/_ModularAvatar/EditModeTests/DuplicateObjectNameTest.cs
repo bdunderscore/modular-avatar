0335c31725a84d737dc462ddebec302eb5e935dc
bd_ <bd_@nadena.dev>
2023-08-20 14:15:10 +0900

    fix: issues with build failures when there are duplicate object paths (#398)

---
+++ Assets/_ModularAvatar/EditModeTests/DuplicateObjectNameTest.cs
@@ -0,0 +1,21 @@
+﻿using modular_avatar_tests;
+using nadena.dev.modular_avatar.core.editor;
+using NUnit.Framework;
+
+namespace _ModularAvatar.EditModeTests
+{
+    public class DuplicateObjectNameTest : TestBase
+    {
+        [Test]
+        public void test_duplicate_object_names()
+        {
+            var avatar = CreateRoot("root");
+            var c1 = CreateChild(avatar, "child");
+            var c2 = CreateChild(avatar, "child");
+
+            PathMappings.Init(avatar);
+            c2.gameObject.name = "child2";
+            Assert.AreEqual(PathMappings.MapPath("child"), "child");
+        }
+    }
+}
