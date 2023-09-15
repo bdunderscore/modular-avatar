694f8ec638ad83d616fb6188e662f47a10e58102
bd_ <bd_@nadena.dev>
2023-09-15 20:02:06 +0900

    fix: workaround issues with double-Armature hack
    fixes: #308

---
+++ Assets/_ModularAvatar/EditModeTests/AvatarObjectReferenceTest.cs
@@ -0,0 +1,21 @@
+﻿using modular_avatar_tests;
+using nadena.dev.modular_avatar.core;
+using NUnit.Framework;
+
+public class AvatarObjectReferenceTest : TestBase
+{
+    [Test]
+    public void whenFakeArmaturePresent_bindsToRealArmature()
+    {
+        var root = CreateRoot("root");
+        var fake_armature = CreateChild(root, "Armature");
+        var real_armature = CreateChild(root, "Armature");
+        
+        var hips = CreateChild(real_armature, "Hips");
+
+        var reference = new AvatarObjectReference();
+        reference.referencePath = "Armature";
+        
+        Assert.AreEqual(real_armature, reference.Get(root.transform));
+    }
+}
