23a802722239d9a33e9bb6d43ed2d6c988afc9f4
bd_ <bd_@nadena.dev>
2023-09-30 16:52:28 +0900

    docs: set trailingSlash: false

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bdunderscore', // Usually your GitHub org/user name.
   projectName: 'modular-avatar', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
