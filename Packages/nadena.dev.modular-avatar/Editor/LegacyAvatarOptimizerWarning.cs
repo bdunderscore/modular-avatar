d35723c34d8e0b60ef43aead489467e308902df8
bd_ <bd_@nadena.dev>
2023-10-07 18:08:15 +0900

    chore: adjust legacy AAO warning

--- Packages/nadena.dev.modular-avatar/Editor/LegacyAvatarOptimizerWarning.cs
+++ Packages/nadena.dev.modular-avatar/Editor/LegacyAvatarOptimizerWarning.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 namespace nadena.dev.modular_avatar.core.editor
 {
     [InitializeOnLoad]
-    public class LegacyAvatarOptimizerWarning
+    internal class LegacyAvatarOptimizerWarning
     {
         static LegacyAvatarOptimizerWarning()
         {
@@ -22,19 +22,19 @@ namespace nadena.dev.modular_avatar.core.editor
                 string message, readInOtherLang;
                 if (isJapanese)
                 {
-                    message = "1.4.x以前のAvatar Optimizerがインストールされているようです。\n" +
-                              "現在お使いのModular Avatarでは、1.4.x以前のAvatar Optimizerと互換性がありません。\n" +
-                              "Avatar Optimizerを1.5.0以降に更新してください！";
+                    message = "1.4.x以前のAvatar Optimizerがインストールされているようです。\n\n" +
+                              "現在お使いのModular Avatarでは、1.4.x以前のAvatar Optimizerと互換性がありません。\n\n" +
+                              "Avatar Optimizerを1.5.0以降に更新してください。";
                     readInOtherLang = "Read in English";
                 }
                 else
                 {
-                    message = "We found Avatar Optimizer 1.4.x or older is installed!\n" +
-                              "This version of Modular Avatar is not compatible with Avatar Optimizer 1.4.x or older!\n" +
-                              "Please upgrade Avatar Optimizer to 1.5.0 or later!";
+                    message = "Avatar Optimizer 1.4.x or older is installed.\n\n" +
+                              "This version of Modular Avatar is not compatible with Avatar Optimizer 1.4.x or older.\n\n" +
+                              "Please upgrade Avatar Optimizer to 1.5.0 or later.";
                     readInOtherLang = "日本語で読む";
                 }
-            
+
                 if (EditorUtility.DisplayDialog("Modular Avatar", message, "OK", readInOtherLang))
                     return;
 
