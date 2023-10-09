f538bf842fed6c247d0ca179af4819643e53ab4b
kaikoga <info@mail.kaikoga.net>
2023-10-09 15:55:27 +0900

    chore: fix SceneManagement editor using declaration on unity 2021 (#483)

--- Editor/Inspector/InspectorCommon.cs
+++ Editor/Inspector/InspectorCommon.cs
@@ -1,5 +1,11 @@
 ﻿using UnityEditor;
+
+#if UNITY_2021_2_OR_NEWER
+using UnityEditor.SceneManagement;
+#else
 using UnityEditor.Experimental.SceneManagement;
+#endif
+
 using UnityEngine;
 
 namespace nadena.dev.modular_avatar.core.editor
