314228adc70fd7f74dd76d8cf0d657a5c8f4ca7e
kaikoga <info@mail.kaikoga.net>
2023-12-30 07:57:46 +0900

    introduce custom clone library

---
+++ Editor/VRM/CustomCloneVRM10Object.cs
@@ -0,0 +1,45 @@
+#if MA_VRM1
+
+using UnityEditor;
+using UnityEngine;
+using UniVRM10;
+
+namespace nadena.dev.modular_avatar.core.editor.vrm
+{
+    class CustomCloneVRM10Object : CustomClone<VRM10Object>
+    {
+        readonly bool _rename;
+        readonly bool _copyThumbnail;
+
+        public CustomCloneVRM10Object(bool rename = false, bool copyThumbnail = false)
+        {
+            _rename = rename;
+            _copyThumbnail = copyThumbnail;
+        }
+        
+        protected override void Define(CopyStrategyDescriptor descriptor)
+        {
+            descriptor.DeepCopy<VRM10Object>(postProcess: Rename);
+            descriptor.DeepCopy<VRM10Expression>(postProcess: Rename);
+
+            if (_copyThumbnail)
+            {
+                descriptor.DeepCopy<Texture2D>();
+            }
+            else
+            {
+                descriptor.ShallowCopy<Texture2D>();
+            }
+            
+            descriptor.ShallowCopy<MonoScript>();
+            descriptor.ShallowCopy<GameObject>();
+
+            void Rename(Object obj)
+            {
+                if (_rename) obj.name = $"{obj.name} (Clone)";
+            }
+        }
+    }
+}
+
+#endif
