5c8852bb14ea2b6bb2e155729fab42b72fce2d00
kaikoga <info@mail.kaikoga.net>
2023-12-30 09:42:19 +0900

    add MA Merge VRM1 SpringBones

---
+++ Editor/VRM/MergeVRM1SpringBonesPass.cs
@@ -0,0 +1,46 @@
+#if MA_VRM1
+
+using System.Linq;
+using nadena.dev.ndmf;
+using nadena.dev.modular_avatar.core.vrm;
+
+using UniVRM10;
+
+namespace nadena.dev.modular_avatar.core.editor.vrm
+{
+    internal class MergeVRM1SpringBonesPass : Pass<MergeVRM1SpringBonesPass>
+    {
+        protected override void Execute(ndmf.BuildContext context)
+        {
+            var processor = new MergeSpringBoneProcessor();
+            processor.ProcessVRM1(context);
+        }
+    }
+
+    public class MergeSpringBoneProcessor
+    {
+        public void ProcessVRM1(ndmf.BuildContext context)
+        {
+            var rootTransform = context.AvatarRootObject;
+            var vrmInstance = rootTransform.GetComponent<Vrm10Instance>();
+            if (!vrmInstance) return;
+
+            var sources = rootTransform.GetComponentsInChildren<ModularAvatarMergeVRM1SpringBones>(); 
+
+            vrmInstance.SpringBone.ColliderGroups = vrmInstance.SpringBone.ColliderGroups
+                .Concat(sources.SelectMany(bone => bone.colliderGroups))
+                .ToList();
+            
+            vrmInstance.SpringBone.Springs = vrmInstance.SpringBone.Springs
+                .Concat(sources.SelectMany(bone => bone.springs))
+                .ToList();
+
+            foreach (var source in sources)
+            {
+                UnityEngine.Object.DestroyImmediate(source);
+            }
+        }
+    }
+}
+
+#endif
