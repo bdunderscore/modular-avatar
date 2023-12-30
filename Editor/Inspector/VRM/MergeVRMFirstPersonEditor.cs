d28231c93dc97ec120e52dae172e1284412cb580
kaikoga <info@mail.kaikoga.net>
2023-12-30 10:27:27 +0900

    add minimal editor

---
+++ Editor/Inspector/VRM/MergeVRMFirstPersonEditor.cs
@@ -0,0 +1,27 @@
+#if MA_VRM0 || MA_VRM1
+
+using nadena.dev.modular_avatar.core.vrm;
+using UnityEditor;
+
+namespace nadena.dev.modular_avatar.core.editor.vrm
+{
+    [CustomEditor(typeof(ModularAvatarMergeVRMFirstPerson))]
+    internal class MergeVRMFirstPersonEditor : MAEditorBase
+    {
+        private SerializedProperty _prop_renderers;
+
+        private void OnEnable()
+        {
+            _prop_renderers = serializedObject.FindProperty(nameof(ModularAvatarMergeVRMFirstPerson.renderers));
+        }
+        
+        protected override void OnInnerInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(_prop_renderers);
+            serializedObject.ApplyModifiedProperties();
+            Localization.ShowLanguageUI();
+        }
+    }
+}
+
+#endif
