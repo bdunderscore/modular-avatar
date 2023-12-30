4aa27c6a9e787fc52436a5e9b807913cd9787bbd
kaikoga <info@mail.kaikoga.net>
2023-12-30 09:54:57 +0900

    add minimal editor

---
+++ Editor/Inspector/VRM/MergeVRM1SpringBonesEditor.cs
@@ -0,0 +1,30 @@
+﻿#if MA_VRM1
+
+using nadena.dev.modular_avatar.core.vrm;
+using UnityEditor;
+
+namespace nadena.dev.modular_avatar.core.editor.vrm
+{
+    [CustomEditor(typeof(ModularAvatarMergeVRM1SpringBones))]
+    internal class MergeVRM1SpringBonesEditor : MAEditorBase
+    {
+        private SerializedProperty _prop_collider_groups;
+        private SerializedProperty _prop_springs;
+
+        private void OnEnable()
+        {
+            _prop_collider_groups = serializedObject.FindProperty(nameof(ModularAvatarMergeVRM1SpringBones.colliderGroups));
+            _prop_springs = serializedObject.FindProperty(nameof(ModularAvatarMergeVRM1SpringBones.springs));
+        }
+        
+        protected override void OnInnerInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(_prop_collider_groups);
+            EditorGUILayout.PropertyField(_prop_springs);
+            serializedObject.ApplyModifiedProperties();
+            Localization.ShowLanguageUI();
+        }
+    }
+}
+
+#endif
