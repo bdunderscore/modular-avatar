42ca90a6bcdd5e975396934579df401cdf966103
bd_ <bd_@nadena.dev>
2024-02-18 21:56:59 +0900

    fix: crash when undoing setup outfit (#680)
    Closes: #571

---
+++ Runtime/ArmatureAwase/DeferDestroy.cs
@@ -0,0 +1,18 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace nadena.dev.modular_avatar.core.armature_lock
+{
+    internal static class DeferDestroy
+    {
+        internal static void DeferDestroyObj(IDisposable obj)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.delayCall += () => obj.Dispose();
+#endif
+        }
+    }
+}
