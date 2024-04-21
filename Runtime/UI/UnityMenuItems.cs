456adac0c5645dc2c23b9b96dbfcff8a36fc23fa
bd_ <bd_@nadena.dev>
2024-04-21 16:49:22 -0700

    feat: add menu option to disable "modular avatar information" display
    Closes: #815

--- Runtime/UI/UnityMenuItems.cs
+++ Runtime/UI/UnityMenuItems.cs
@@ -5,10 +5,14 @@
         internal const string TopMenu_EditModeBoneSync = "Tools/Modular Avatar/Sync Bones in Edit Mode";
         internal const int TopMenu_EditModeBoneSyncOrder = 100;
         
+        internal const string TopMenu_EnableInfo = "Tools/Modular Avatar/Show Modular Avatar Information";
+        internal const int TopMenu_EnableInfoOrder = 101;
+        
         internal const string TopMenu_ManualBakeAvatar = "Tools/Modular Avatar/Manual Bake Avatar";
         internal const int TopMenu_ManualBakeAvatarOrder = 1000;
         
         internal const string TopMenu_ReloadLocalizations = "Tools/Modular Avatar/Reload Localizations";
         internal const int TopMenu_ReloadLocalizationsOrder = 1001;
+
     }
 }
