56afe04410b7b0e1db3cf37559808aab69ddc9b2
bd_ <bd_@nadena.dev>
2024-08-05 20:26:06 -0700

    fix: shape changer would not generate animations when controlled only by object toggle

--- Editor/ReactiveObjects/ControlCondition.cs
+++ Editor/ReactiveObjects/ControlCondition.cs
@@ -1,10 +1,15 @@
-﻿namespace nadena.dev.modular_avatar.core.editor
+﻿using UnityEngine;
+
+namespace nadena.dev.modular_avatar.core.editor
 {
-    internal struct ControlCondition
+    internal class ControlCondition
     {
         public string Parameter, DebugName;
         public bool IsConstant;
         public float ParameterValueLo, ParameterValueHi, InitialValue;
         public bool InitiallyActive => InitialValue > ParameterValueLo && InitialValue < ParameterValueHi;
+        public bool IsConstantActive => InitiallyActive && IsConstant;
+
+        public GameObject ReferenceObject;
     }
 }
