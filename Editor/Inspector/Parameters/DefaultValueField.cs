b6b67c0e0df9f4e6cab7d1db4e89ba6af27120b2
nekobako <nekobako.tofu@gmail.com>
2024-09-14 13:17:34 +0900

    fix: format default value when update type or value on parameters inspector

--- Editor/Inspector/Parameters/DefaultValueField.cs
+++ Editor/Inspector/Parameters/DefaultValueField.cs
@@ -1,6 +1,4 @@
 ﻿using System.Globalization;
-using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,112 +13,102 @@ namespace nadena.dev.modular_avatar.core.editor
         private const string V_None = "　";
         private const string V_True = "ON";
         private const string V_False = "OFF";
-        
+
         private readonly FloatField _defaultValueField;
-        private readonly Toggle _hasExplicitDefaultSetField;
-        private readonly TextField _notSyncedField;
-        private readonly TextField _intField;
-        private readonly TextField _floatField;
+        private readonly Toggle _hasExplicitDefaultValueField;
+        private readonly TextField _numberField;
         private readonly DropdownField _boolField;
+
+        private ParameterSyncType _syncType;
 
         public DefaultValueField()
         {
             // Hidden binding elements
             _defaultValueField = new FloatField();
             _defaultValueField.style.display = DisplayStyle.None;
-            _hasExplicitDefaultSetField = new Toggle();
-            _hasExplicitDefaultSetField.style.display = DisplayStyle.None;
-
-            _defaultValueField.RegisterValueChangedCallback(
-                evt => UpdateVisibleField(evt.newValue, _hasExplicitDefaultSetField.value));
             _defaultValueField.bindingPath = nameof(ParameterConfig.defaultValue);
-            
-            _hasExplicitDefaultSetField.RegisterValueChangedCallback(
-                evt => UpdateVisibleField(_defaultValueField.value, evt.newValue));
-            _hasExplicitDefaultSetField.bindingPath = nameof(ParameterConfig.hasExplicitDefaultValue);
+            _defaultValueField.RegisterValueChangedCallback(evt => UpdateVisibleField(evt.newValue, _hasExplicitDefaultValueField.value));
+            _hasExplicitDefaultValueField = new Toggle();
+            _hasExplicitDefaultValueField.style.display = DisplayStyle.None;
+            _hasExplicitDefaultValueField.bindingPath = nameof(ParameterConfig.hasExplicitDefaultValue);
+            _hasExplicitDefaultValueField.RegisterValueChangedCallback(evt => UpdateVisibleField(_defaultValueField.value, evt.newValue));
 
             // Visible elements for input
-            _notSyncedField = new TextField();
-            _notSyncedField.name = "default-value-not-synced";
-            _notSyncedField.isDelayed = true;
-            _intField = new TextField();
-            _intField.name = "default-value-int";
-            _intField.isDelayed = true;
-            _floatField = new TextField();
-            _floatField.name = "default-value-float";
-            _floatField.isDelayed = true;
+            _numberField = new TextField();
+            _numberField.isDelayed = true;
+            _numberField.RegisterValueChangedCallback(evt => OnUpdateNumberValue(evt.newValue));
             _boolField = new DropdownField();
-            _boolField.name = "default-value-bool";
             _boolField.choices.Add(V_None);
             _boolField.choices.Add(V_True);
             _boolField.choices.Add(V_False);
+            _boolField.RegisterValueChangedCallback(evt => OnUpdateBoolValue(evt.newValue));
 
-            void NumberChangedCallback(ChangeEvent<string> evt)
-            {
-                if (string.IsNullOrWhiteSpace(evt.newValue))
-                {
-                    _defaultValueField.value = 0;
-                    _hasExplicitDefaultSetField.value = false;
-                }
-                else if (float.TryParse(evt.newValue, NumberStyles.Float, CultureInfo.InvariantCulture, out var value))
-                {
-                    _defaultValueField.value = value;
-                    _hasExplicitDefaultSetField.value = true;
-                }
-                else
-                {
-                    UpdateVisibleField(_defaultValueField.value, _hasExplicitDefaultSetField.value);
-                }
-            }
-
-            _notSyncedField.RegisterValueChangedCallback(NumberChangedCallback);
-
-            _intField.RegisterValueChangedCallback(NumberChangedCallback);
-
-            _floatField.RegisterValueChangedCallback(NumberChangedCallback);
-
-            _boolField.RegisterValueChangedCallback(evt =>
-            {
-                _defaultValueField.value = evt.newValue == V_True ? 1 : 0;
-                _hasExplicitDefaultSetField.value = evt.newValue != V_None;
-            });
-            
             Add(_defaultValueField);
-            Add(_hasExplicitDefaultSetField);
-            Add(_notSyncedField);
-            Add(_intField);
-            Add(_floatField);
+            Add(_hasExplicitDefaultValueField);
+            Add(_numberField);
             Add(_boolField);
         }
 
-        public void ManualBindProperty(SerializedProperty property)
+        public void OnUpdateSyncType(ParameterSyncType syncType)
         {
-            _defaultValueField.BindProperty(property);
-            _hasExplicitDefaultSetField.BindProperty(property);
-        }
-        
-        private void UpdateVisibleField(float value, bool hasExplicitValue)
-        {
-            if (Mathf.Abs(value) > 0.0000001)
+            _syncType = syncType;
+
+            if (syncType != ParameterSyncType.Bool)
             {
-                hasExplicitValue = true;
+                _numberField.style.display = DisplayStyle.Flex;
+                _boolField.style.display = DisplayStyle.None;
+                OnUpdateNumberValue(_numberField.value);
+            }
+            else
+            {
+                _numberField.style.display = DisplayStyle.None;
+                _boolField.style.display = DisplayStyle.Flex;
+                OnUpdateBoolValue(_boolField.value);
+            }
+        }
+
+        private void OnUpdateNumberValue(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                _defaultValueField.value = 0;
+                _hasExplicitDefaultValueField.value = false;
+            }
+            else if (float.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsed))
+            {
+                _defaultValueField.value = _syncType switch
+                {
+                    ParameterSyncType.Int => Mathf.FloorToInt(Mathf.Clamp(parsed, 0, 255)),
+                    ParameterSyncType.Float => Mathf.Clamp(parsed, -1, 1),
+                    ParameterSyncType.Bool => parsed != 0 ? 1 : 0,
+                    _ => parsed,
+                };
+                _hasExplicitDefaultValueField.value = true;
             }
 
-            _notSyncedField.SetValueWithoutNotify(hasExplicitValue
-                ? value.ToString(CultureInfo.InvariantCulture)
-                : string.Empty);
+            UpdateVisibleField(_defaultValueField.value, _hasExplicitDefaultValueField.value);
+        }
 
-            _intField.SetValueWithoutNotify(hasExplicitValue
-                ? Mathf.FloorToInt(Mathf.Clamp(value, 0, 255)).ToString(CultureInfo.InvariantCulture)
-                : string.Empty);
+        private void OnUpdateBoolValue(string value)
+        {
+            _defaultValueField.value = value == V_True ? 1 : 0;
+            _hasExplicitDefaultValueField.value = value != V_None;
 
-            _floatField.SetValueWithoutNotify(hasExplicitValue
-                ? Mathf.Clamp(value, -1, 1).ToString(CultureInfo.InvariantCulture)
-                : string.Empty);
+            UpdateVisibleField(_defaultValueField.value, _hasExplicitDefaultValueField.value);
+        }
 
-            _boolField.SetValueWithoutNotify(hasExplicitValue
-                ? value != 0 ? V_True : V_False
-                : V_None);
+        private void UpdateVisibleField(float value, bool hasExplicitValue)
+        {
+            if (hasExplicitValue || Mathf.Abs(value) > 0.0000001)
+            {
+                _numberField.SetValueWithoutNotify(value.ToString(CultureInfo.InvariantCulture));
+                _boolField.SetValueWithoutNotify(value != 0 ? V_True : V_False);
+            }
+            else
+            {
+                _numberField.SetValueWithoutNotify(string.Empty);
+                _boolField.SetValueWithoutNotify(V_None);
+            }
         }
     }
 }
