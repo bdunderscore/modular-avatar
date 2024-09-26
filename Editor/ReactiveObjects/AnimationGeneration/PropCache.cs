9066a6d193d1821bc00e03adb2817caaf40e959c
bd_ <bd_@nadena.dev>
2024-09-25 19:29:56 -0700

    chore: improve PropCache debuggability by adding a name property

--- Editor/ReactiveObjects/AnimationGeneration/PropCache.cs
+++ Editor/ReactiveObjects/AnimationGeneration/PropCache.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using nadena.dev.ndmf.preview;
-using UnityEngine;
 
 namespace nadena.dev.modular_avatar.core.editor
 {
@@ -13,21 +12,25 @@ namespace nadena.dev.modular_avatar.core.editor
             public PropCache<Key, Value> Owner;
             public Key Key;
             public Value Value;
+            public string DebugName;
         }
 
+        private readonly string _debugName;
         private readonly Func<ComputeContext, Key, Value> _operator;
         private readonly Func<Value, Value, bool> _equalityComparer;
         private readonly Dictionary<Key, CacheEntry> _cache = new();
 
-        public PropCache(Func<ComputeContext, Key, Value> operatorFunc, Func<Value, Value, bool> equalityComparer = null)
+        public PropCache(string debugName, Func<ComputeContext, Key, Value> operatorFunc,
+            Func<Value, Value, bool> equalityComparer = null)
         {
+            _debugName = debugName;
             _operator = operatorFunc;
             _equalityComparer = equalityComparer;
         }
         
         private static void InvalidateEntry(CacheEntry entry)
         {
-            var newGenContext = new ComputeContext("PropCache for key " + entry.Key);
+            var newGenContext = new ComputeContext("PropCache/" + entry.DebugName + " key " + entry.Key);
             var newValue = entry.Owner._operator(newGenContext, entry.Key);
             if (entry.Owner._equalityComparer != null && entry.Owner._equalityComparer(entry.Value, newValue))
             {
@@ -44,14 +47,15 @@ namespace nadena.dev.modular_avatar.core.editor
         {
             if (!_cache.TryGetValue(key, out var entry) || entry.GenerateContext.IsInvalidated)
             {
-                var subContext = new ComputeContext("PropCache for key " + key);
+                var subContext = new ComputeContext("PropCache/" + _debugName + " key " + key);
                 entry = new CacheEntry
                 {
                     GenerateContext = subContext,
                     ObserverContext = new ComputeContext("Observer for PropCache for key " + key),
                     Owner = this,
                     Key = key,
-                    Value = _operator(subContext, key)
+                    Value = _operator(subContext, key),
+                    DebugName = _debugName
                 };
                 _cache[key] = entry;
                 
