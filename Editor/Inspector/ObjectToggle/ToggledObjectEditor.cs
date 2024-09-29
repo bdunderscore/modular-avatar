cc93c7e5ed0a197c60e82bc5c70ebf90a44b02c8
nekobako <nekobako.tofu@gmail.com>
2024-09-29 19:06:28 +0900

    feat: replace checkboxes in ObjectToggle with dropdowns

--- Editor/Inspector/ObjectToggle/ToggledObjectEditor.cs
+++ Editor/Inspector/ObjectToggle/ToggledObjectEditor.cs
@@ -15,6 +15,9 @@ namespace nadena.dev.modular_avatar.core.editor.ShapeChanger
         private const string UxmlPath = Root + "ToggledObjectEditor.uxml";
         private const string UssPath = Root + "ObjectSwitcherStyles.uss";
 
+        private const string V_On = "ON";
+        private const string V_Off = "OFF";
+
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
             var uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UxmlPath).CloneTree();
@@ -23,6 +26,21 @@ namespace nadena.dev.modular_avatar.core.editor.ShapeChanger
             Localization.UI.Localize(uxml);
             uxml.styleSheets.Add(uss);
             uxml.BindProperty(property);
+
+            var f_active = uxml.Q<Toggle>("f-active");
+            var f_active_dropdown = uxml.Q<DropdownField>("f-active-dropdown");
+
+            f_active_dropdown.choices.Add(V_On);
+            f_active_dropdown.choices.Add(V_Off);
+
+            f_active.RegisterValueChangedCallback(evt =>
+            {
+                f_active_dropdown.SetValueWithoutNotify(evt.newValue ? V_On : V_Off);
+            });
+            f_active_dropdown.RegisterValueChangedCallback(evt =>
+            {
+                f_active.value = evt.newValue == V_On;
+            });
 
             return uxml;
         }
