656a4016842c3216c2bf807fddd0016b076dc0d5
bd_ <bd_@nadena.dev>
2024-10-05 15:19:42 -0700

    fix: proxy animations are cloned in Merge Animator (#1266)
    This fixes issues with しゃがみ置き換え＋α among other things.

---
+++ UnitTests~/MergeAnimatorTests/ProxyAnim/ProxyAnimTest.cs
@@ -0,0 +1,28 @@
+﻿using modular_avatar_tests;
+using nadena.dev.modular_avatar.core;
+using nadena.dev.modular_avatar.core.editor;
+using NUnit.Framework;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace UnitTests.MergeAnimatorTests.ProxyAnim
+{
+    public class ProxyAnimTest : TestBase
+    {
+        [Test]
+        public void whenProxyAnimIsMerged_itIsNotReplaced()
+        {
+            var root = CreatePrefab("ProxyAnimTest.prefab");
+
+            var originalAnimator = (AnimatorController) root.GetComponentInChildren<ModularAvatarMergeAnimator>().animator;
+            var originalClip = originalAnimator.layers[0].stateMachine.states[0].state.motion as AnimationClip;
+            
+            AvatarProcessor.ProcessAvatar(root);
+            
+            var resultLayer = findFxLayer(root, "proxy");
+            var resultClip = resultLayer.stateMachine.states[0].state.motion as AnimationClip;
+            
+            Assert.AreEqual(originalClip, resultClip);
+        }
+    }
+}
