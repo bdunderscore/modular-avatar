b8c2a6045e52762b9409f687af2ee05f3e6edd68
bd_ <bd_@nadena.dev>
2024-11-21 19:39:54 -0800

    fix: multiple issues with MAMoveIndependently
    Fixed issues with nested Move Independently components (Closes #1367).
    
    Fixed MAMoveIndependently not being saved (Supercedes #1366)
    
    Reduce jittering when moving MAMI bones.

--- Runtime/MAMoveIndependently.cs
+++ Runtime/MAMoveIndependently.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using nadena.dev.modular_avatar.core.armature_lock;
 using UnityEditor;
 using UnityEngine;
 #if MA_VRCSDK3_AVATARS
@@ -22,229 +20,33 @@ namespace nadena.dev.modular_avatar.core.ArmatureAwase
 
         public GameObject[] GroupedBones
         {
-            get => m_groupedBones.Clone() as GameObject[];
+            get => m_groupedBones?.Clone() as GameObject[] ?? Array.Empty<GameObject>();
             set
             {
                 m_groupedBones = value.Clone() as GameObject[];
-                OnValidate();
+                MaMoveIndependentlyManager.Instance.Activate(this);
             }
         }
-
-        struct ChildState
-        {
-            internal Vector3 childLocalPos;
-            internal Quaternion childLocalRot;
-            internal Vector3 childLocalScale;
-
-            // The child world position, recorded when we first initialized (or after unexpected child movement)
-            internal Matrix4x4 childToRoot;
-        }
-
-        private Dictionary<Transform, ChildState> _children = new Dictionary<Transform, ChildState>();
-        private HashSet<Transform> _excluded = new HashSet<Transform>();
-
-        void Awake()
-        {
-            hideFlags = HideFlags.DontSave;
-        }
-
-        // We need to reparent the TRS values of the children from our prior frame state to the current frame state.
-        // This is done by computing the world affine matrix for the child in the prior frame, then converting to
-        // a local affine matrix in the current frame.
 
         private void OnValidate()
         {
-            hideFlags = HideFlags.DontSave;
-            _excluded = new HashSet<Transform>();
-            if (m_groupedBones == null)
+            if (!PrefabUtility.IsPartOfPrefabAsset(this))
             {
-                m_groupedBones = Array.Empty<GameObject>();
-            }
-
-            foreach (var grouped in m_groupedBones)
-            {
-                if (grouped != null)
+                EditorApplication.delayCall += () =>
                 {
-                    _excluded.Add(grouped.transform);
-                }
-            }
-
-            _priorFramePos = transform.localPosition;
-            _priorFrameRot = transform.localRotation;
-            _priorFrameScale = transform.localScale;
-
-            _children.Clear();
-            CheckChildren();
-        }
-
-        HashSet<Transform> _observed = new HashSet<Transform>();
-
-        private void CheckChildren()
-        {
-            _observed.Clear();
-
-            CheckChildren(transform);
-            foreach (var obj in m_groupedBones)
-            {
-                CheckChildren(obj.transform);
-            }
-
-            // Remove any children that are no longer children
-            var toRemove = new List<Transform>();
-            foreach (var child in _children)
-            {
-                if (child.Key == null || !_observed.Contains(child.Key))
-                {
-                    toRemove.Add(child.Key);
-                }
-            }
-
-            foreach (var child in toRemove)
-            {
-                _children.Remove(child);
-            }
-        }
-
-        private Matrix4x4 ParentTransformMatrix(Transform parent)
-        {
-            Matrix4x4 transform = Matrix4x4.TRS(
-                parent.localPosition,
-                parent.localRotation,
-                parent.localScale
-            );
-
-            if (_excluded.Contains(parent))
-            {
-                transform = ParentTransformMatrix(parent.parent) * transform;
-            }
-
-            return transform;
-        }
-
-        private void CheckChildren(Transform parent)
-        {
-            Matrix4x4 parentToRoot = ParentTransformMatrix(parent);
-            Matrix4x4 rootToParent = parentToRoot.inverse;
-
-            foreach (Transform child in parent)
-            {
-                if (_excluded.Contains(child)) continue;
-
-                _observed.Add(child);
-
-                var localPosition = child.localPosition;
-                var localRotation = child.localRotation;
-                var localScale = child.localScale;
-
-                if (!ArmatureLockController.MovedThisFrame && _children.TryGetValue(child, out var state))
-                {
-                    var deltaPos = localPosition - state.childLocalPos;
-                    var deltaRot = Quaternion.Angle(localRotation, state.childLocalRot);
-                    var deltaScale = (localScale - state.childLocalScale).sqrMagnitude;
-
-                    if (deltaPos.magnitude > EPSILON || deltaRot > EPSILON || deltaScale > EPSILON)
-                    {
-                        // The child object was moved in between parent updates; reconstruct its childToRoot to correct
-                        // for this.
-                        var oldChildTRS = Matrix4x4.TRS(
-                            state.childLocalPos,
-                            state.childLocalRot,
-                            state.childLocalScale
-                        );
-
-                        var newChildTRS = Matrix4x4.TRS(
-                            localPosition,
-                            localRotation,
-                            localScale
-                        );
-
-                        state.childToRoot = state.childToRoot * oldChildTRS.inverse * newChildTRS;
-                    }
-
-                    Matrix4x4 childNewLocal = rootToParent * state.childToRoot;
-
-                    var newPosition = childNewLocal.MultiplyPoint(Vector3.zero);
-                    var newRotation = childNewLocal.rotation;
-                    var newScale = childNewLocal.lossyScale;
-#if UNITY_EDITOR
-                    Undo.RecordObject(child, Undo.GetCurrentGroupName());
-#endif
-
-                    child.localPosition = newPosition;
-                    child.localRotation = newRotation;
-                    child.localScale = newScale;
-
-                    state.childLocalPos = child.localPosition;
-                    state.childLocalRot = child.localRotation;
-                    state.childLocalScale = child.localScale;
-
-                    _children[child] = state;
-
-                    continue;
-                }
-
-                Matrix4x4 childTRS = Matrix4x4.TRS(localPosition, localRotation, localScale);
-
-                state = new ChildState()
-                {
-                    childLocalPos = localPosition,
-                    childLocalRot = localRotation,
-                    childLocalScale = localScale,
-                    childToRoot = parentToRoot * childTRS,
+                    if (this != null) MaMoveIndependentlyManager.Instance.Activate(this);
                 };
-
-                _children[child] = state;
             }
         }
 
         private void OnEnable()
         {
-            UpdateLoopController.OnMoveIndependentlyUpdate += OnUpdate;
+            MaMoveIndependentlyManager.Instance.Activate(this);
         }
 
         private void OnDisable()
         {
-            UpdateLoopController.OnMoveIndependentlyUpdate -= OnUpdate;
-        }
-
-        private Vector3 _priorFramePos, _priorFrameScale;
-        private Quaternion _priorFrameRot;
-
-        void OnUpdate()
-        {
-            if (this == null)
-            {
-                UpdateLoopController.OnMoveIndependentlyUpdate -= OnUpdate;
-                return;
-            }
-
-            if (transform.parent == null) return;
-            
-            var pos = transform.localPosition;
-            var rot = transform.localRotation;
-            var scale = transform.localScale;
-
-            var deltaPos = transform.parent.localToWorldMatrix.MultiplyVector(pos - _priorFramePos);
-            var deltaRot = Quaternion.Angle(rot, _priorFrameRot);
-
-            var deltaScaleX = Mathf.Abs((scale - _priorFrameScale).x) / _priorFrameScale.x;
-            var deltaScaleY = Mathf.Abs((scale - _priorFrameScale).y) / _priorFrameScale.y;
-            var deltaScaleZ = Mathf.Abs((scale - _priorFrameScale).z) / _priorFrameScale.z;
-
-            if (float.IsNaN(deltaScaleX) || float.IsInfinity(deltaScaleX)) deltaScaleX = 1;
-            if (float.IsNaN(deltaScaleY) || float.IsInfinity(deltaScaleY)) deltaScaleY = 1;
-            if (float.IsNaN(deltaScaleZ) || float.IsInfinity(deltaScaleZ)) deltaScaleZ = 1;
-
-            float maxDeltaScale = Mathf.Max(deltaScaleX, Mathf.Max(deltaScaleY, deltaScaleZ));
-
-            if (deltaPos.magnitude > EPSILON || deltaRot > EPSILON || maxDeltaScale > 0.001)
-            {
-                CheckChildren();
-
-                _priorFramePos = pos;
-                _priorFrameRot = rot;
-                _priorFrameScale = scale;
-            }
+            MaMoveIndependentlyManager.Instance.Deactivate(this);
         }
     }
 }
