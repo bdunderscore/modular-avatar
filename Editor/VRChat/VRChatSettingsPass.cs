70f77b08a498a98ed8b2901986a721e3d89f9843
bd_ <bd_@nadena.dev>
2025-03-22 19:38:21 -0700

    feat: allow opt-out from MMD handling
    Closes: #1518

---
+++ Editor/VRChat/VRChatSettingsPass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using nadena.dev.modular_avatar.editor.ErrorReporting;
+using nadena.dev.ndmf;
+
+namespace nadena.dev.modular_avatar.core.editor
+{
+    internal class VRChatSettingsPass : Pass<VRChatSettingsPass>
+    {
+        protected override void Execute(ndmf.BuildContext context)
+        {
+            var settings = context.AvatarRootObject.GetComponentsInChildren<ModularAvatarVRChatSettings>(true);
+
+            if (settings.Length > 1)
+            {
+                var objects = new List<object>();
+                objects.Add("MA VRChat Settings");
+                objects.AddRange(settings);
+
+                BuildReport.LogFatal("error.singleton", objects);
+            }
+        }
+    }
+}
---
+++ Runtime/ModularAvatarVRChatSettings.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace nadena.dev.modular_avatar.core
+{
+    [AddComponentMenu("Modular Avatar/MA VRChat Settings")]
+    [DisallowMultipleComponent]
+    [HelpURL("https://modular-avatar.nadena.dev/docs/reference/vrchat-settings?lang=auto")]
+    public class ModularAvatarVRChatSettings : AvatarTagComponent
+    {
+        [SerializeField] internal bool m_mmdWorldSupport = true;
+
+        [PublicAPI]
+        public bool MMDWorldSupport
+        {
+            get => m_mmdWorldSupport;
+            set => m_mmdWorldSupport = value;
+        }
+    }
+}
