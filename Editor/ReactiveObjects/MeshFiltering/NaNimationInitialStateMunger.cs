207ed92bf2e47edd47422507456a2590f410e765
bd_ <bd_@nadena.dev>
2025-08-09 15:07:35 -0700

    fix: NaNimation initial states are not applied when animations are inactive
    This fixes #1657 in a way which does not trigger VRChat crashes in
    certain worlds. Unlike #1660, which used infinite blendshape
    displacement to erase polygons (which has side effects such as crashing,
    and causing issues with orthographic cameras), we instead revert to
    using NaNimation, and use VRC Scale Constraints to NaNimate the initial
    state of our NaNimation bones.

---
+++ Editor/ReactiveObjects/MeshFiltering/NaNimationInitialStateMunger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using nadena.dev.ndmf.animator;
+using UnityEditor;
+using UnityEngine;
+using VRC.Dynamics;
+using VRC.SDK3.Dynamics.Constraint.Components;
+
+#if MA_VRCSDK3_AVATARS
+
+namespace nadena.dev.modular_avatar.core.editor
+{
+    /// <summary>
+    /// Configures the initial state of NaNimation bones.
+    ///
+    /// Because we cannot configure the initial scale of a bone to be NaN, we instead use VRCScaleConstraints
+    /// to animate those bones to NaN when user animations are disabled. When user animations are enabled,
+    /// we turn off the constraints, which allows normal NaNimations to take over.
+    /// </summary>
+    internal static class NaNimationInitialStateMunger
+    {
+        public static void ApplyInitialStates(
+            Transform avatarRoot,
+            IEnumerable<GameObject> initiallyActive,
+            VirtualClip baseStateClip
+        )
+        {
+            foreach (var initialBone in initiallyActive)
+            {
+                if (initialBone == null) continue;
+
+                var constraint = initialBone.AddComponent<VRCScaleConstraint>();
+                constraint.Sources.Add(new VRCConstraintSource
+                {
+                    SourceTransform = constraint.transform,
+                    Weight = float.NaN
+                });
+                constraint.GlobalWeight = 1.0f;
+                constraint.Locked = true;
+                constraint.IsActive = true;
+
+                baseStateClip.SetFloatCurve(
+                    EditorCurveBinding.FloatCurve(
+                        RuntimeUtil.AvatarRootPath(constraint.gameObject),
+                        typeof(VRCScaleConstraint),
+                        "IsActive"
+                    ),
+                    // Disable the constraint when animations are active
+                    AnimationCurve.Constant(0, 1, 0.0f)
+                );
+            }
+        }
+    }
+}
+
+#endif
