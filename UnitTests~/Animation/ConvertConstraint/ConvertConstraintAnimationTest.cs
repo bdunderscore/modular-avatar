b381bfbaf71dea82a79e3df6446580d4eddf4448
bd_ <bd_@nadena.dev>
2025-11-02 16:54:20 -0800

    fix: Convert Constraints breaks constraints replaced with Replace Object

---
+++ UnitTests~/Animation/ConvertConstraint/ConvertConstraintAnimationTest.cs
@@ -0,0 +1,31 @@
+﻿#if MA_VRCSDK3_AVATARS
+
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.platform;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Dynamics.Constraint.Components;
+
+namespace modular_avatar_tests.ConvertConstraint
+{
+    internal class ConvertConstraintAnimationTest : TestBase
+    {
+        [Test]
+        public void WhenReplacedObjectIsConverted_AnimationIsAdjusted()
+        {
+            var prefab = CreatePrefab("WhenReplacedObjectIsConverted_AnimationIsAdjusted.prefab");
+
+            AvatarProcessor.ProcessAvatar(prefab,
+                PlatformRegistry.PlatformProviders[WellKnownPlatforms.VRChatAvatar30]
+            );
+
+            var motion = (AnimationClip) findFxMotion(prefab, "test");
+            var ecb = AnimationUtility.GetCurveBindings(motion)[0];
+            
+            Assert.AreEqual(ecb.type, typeof(VRCAimConstraint));
+        }
+    }
+}
+
+#endif
