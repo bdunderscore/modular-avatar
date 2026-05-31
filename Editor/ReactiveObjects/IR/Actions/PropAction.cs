1d26ce21f4db55a9d1e014a09e77de5e9b8f4cba
bd_ <bd_@nadena.dev>
2026-05-31 13:30:44 -0700

    pptr curve support

--- Editor/ReactiveObjects/IR/Actions/PropAction.cs
+++ Editor/ReactiveObjects/IR/Actions/PropAction.cs
@@ -20,13 +20,23 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
 
         public void ToMotion(BakeContext context, VirtualClip motion)
         {
-            // TODO - handle non-float values
+            var binding = GetCurveBinding(context);
+            if (binding == null) return;
+            
             if (_value is float f)
             {
-                var binding = GetCurveBinding(context);
-                if (binding == null) return;
-
                 motion.SetFloatCurve(binding.Value, AnimationCurve.Constant(0, 1, f));
+            }
+            else if (_value is Object obj)
+            {
+                motion.SetObjectCurve(binding.Value, new[]
+                {
+                    new ObjectReferenceKeyframe
+                    {
+                        time = 0,
+                        value = obj
+                    }
+                });
             }
         }
 
@@ -39,11 +49,22 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
             else if (obj is Component c) gameObject = c.gameObject;
             else return null;
 
-            return EditorCurveBinding.FloatCurve(
-                context.ObjectPathRemapper.GetVirtualPathForObject(gameObject),
-                _prop.TargetObject.GetType(),
-                _prop.PropertyName
-            );
+            if (_value is float)
+            {
+                return EditorCurveBinding.FloatCurve(
+                    context.ObjectPathRemapper.GetVirtualPathForObject(gameObject),
+                    _prop.TargetObject.GetType(),
+                    _prop.PropertyName
+                );
+            }
+            else
+            {
+                return EditorCurveBinding.PPtrCurve(
+                    context.ObjectPathRemapper.GetVirtualPathForObject(gameObject),
+                    _prop.TargetObject.GetType(),
+                    _prop.PropertyName
+                );
+            }
         }
 
 
@@ -54,7 +75,7 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
             var binding = GetCurveBinding(context);
             if (!binding.HasValue) return;
 
-            float? originalValue = null;
+            object? originalValue = null;
 
             if (targetObject is SkinnedMeshRenderer smr && _prop.PropertyName.StartsWith("blendShape."))
             {
@@ -71,7 +92,9 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
             else
             {
                 var so = new SerializedObject(_prop.TargetObject);
-                var staticState = (float)_value;
+                var staticState = _value;
+
+                if (staticState == null) return;
 
                 // TODO - staticStateOverrides
                 var prop = so.FindProperty(_prop.PropertyName);
@@ -81,11 +104,15 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
                     {
                         case SerializedPropertyType.Boolean:
                             originalValue = prop.boolValue ? 1.0f : 0.0f;
-                            prop.boolValue = staticState > 0.5f;
+                            prop.boolValue = (float)staticState > 0.5f;
                             break;
                         case SerializedPropertyType.Float:
                             originalValue = prop.floatValue;
-                            prop.floatValue = staticState;
+                            prop.floatValue = (float)staticState;
+                            break;
+                        case SerializedPropertyType.ObjectReference:
+                            originalValue = prop.objectReferenceValue;
+                            prop.objectReferenceValue = (Object)staticState;
                             break;
                         default:
                             // TODO - objectReference
@@ -94,12 +121,26 @@ namespace nadena.dev.modular_avatar.core.editor.rc.Actions
                 }
             }
 
-            if (originalValue.HasValue)
+            // We always set to the original active state to give other layers a chance to override
+            if (originalValue is float f)
             {
-                // We always set to the original active state to give other layers a chance to override
                 context.BaseLayerClip.SetFloatCurve(
                     binding.Value,
-                    AnimationCurve.Constant(0, 1, originalValue.Value)
+                    AnimationCurve.Constant(0, 1, f)
+                );
+            }
+            else if (originalValue is Object obj)
+            {
+                context.BaseLayerClip.SetObjectCurve(
+                    binding.Value,
+                    new[]
+                    {
+                        new ObjectReferenceKeyframe
+                        {
+                            time = 0,
+                            value = obj
+                        }
+                    }
                 );
             }
         }
