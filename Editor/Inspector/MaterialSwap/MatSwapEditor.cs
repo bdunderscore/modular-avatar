1dbc95babece53648d46c91ae037dff583977619
bd_ <bd_@nadena.dev>
2026-06-21 15:02:19 -0700

    fix: compile error on Unity 6000.0 (but not .4)

--- Editor/Inspector/MaterialSwap/MatSwapEditor.cs
+++ Editor/Inspector/MaterialSwap/MatSwapEditor.cs
@@ -101,7 +101,11 @@ namespace nadena.dev.modular_avatar.core.editor
 #if UNITY_6000_3_OR_NEWER
                 menu.DropDown(_fromField.worldBound, _fromField,DropdownMenuSizeMode.Auto);
 #else
+#if UNITY_6000_0_OR_NEWER
+                menu.DropDown(_fromField.worldBound, _fromField, false, false);
+#else
                 menu.DropDown(_fromField.worldBound, _fromField);
+#endif
 #endif
             };
         }
